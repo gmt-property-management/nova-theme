9d39c8354044109a252beb6c5e598de1eeb552b4
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2020-05-10 12:11:00 +0800

    Initial commit.

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Gmt\NovaTheme;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::theme(asset('gmt/nova-theme/theme.css'));
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('gmt/nova-theme'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
